4fca4d8c899d52827ec9f6f1ba8a3463bebd3936
buicongthanh861 <buicongthanh861@gmail.com>
2026-08-14 02:37:28 +0700

    fix error oidc aws

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ function App() {
     return (
         <div className="app">
             <div className="navbar">
-                <div className="brand">Cong Thanh DevOps</div>
+                <div className="brand">Cong Thanh DevOps123</div>
                 <div className="nav-links">
                     <a className="home" href="/">Home</a>
                     <a className="youtube" href="https://www.youtube.com/@devopsshack/videos">YouTube</a>

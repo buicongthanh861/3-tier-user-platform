b7075d0d23c87703ae49ffc071534b2caef23f15
buicongthanh861 <buicongthanh861@gmail.com>
2026-08-16 22:06:13 +0700

    fix: configure gp2 storage for mysql

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -28,7 +28,7 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "7.7.0"
+  version    = "10.1.3"
   namespace  = kubernetes_namespace.argocd.metadata[0].name
   create_namespace = true
 

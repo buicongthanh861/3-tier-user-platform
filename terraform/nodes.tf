8183889912cc8bcbfeae9fc0c46a8b90a148c07a
buicongthanh861 <buicongthanh861@gmail.com>
2026-08-16 21:42:22 +0700

    test full cicd + argocd

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -44,12 +44,12 @@ resource "aws_eks_node_group" "general" {
   ]
 
   capacity_type = "ON_DEMAND"
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 2
-    max_size     = 3
-    min_size     = 1
+    max_size     = 5
+    min_size     = 2
   }
 
   lifecycle {
